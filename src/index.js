56992af85d618249060dfe3d754f16d5307be085
fj <filip.jovakaric@gmail.com>
2016-08-16 23:22:58 +0200

    Add react-bootstrap to project #18

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Router, browserHistory } from 'react-router';
 
 import routes from './routes';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 render (
     <Router history={browserHistory} routes={routes} />,
     document.getElementById('redux-omdb-example')
