b8a902331c723ae3abff5d890a204c02998396ec
fj <filip.jovakaric@gmail.com>
2016-08-16 23:44:22 +0200

    Add global styles #22

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './main.scss';
 
 render (
     <Router history={browserHistory} routes={routes} />,
