a3b06912d80fcefd587b2ef497bfa0556b308116
fj <filip.jovakaric@gmail.com>
2016-08-16 23:09:20 +0200

    Create HomePage component #15

---
+++ src/pages/home/home.container.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="home-page">
+                <h1> This is the home page </h1>
+            </div>
+        );
+    }
+}
+
+HomePage.propTypes = {
+};
+
+export default HomePage;
