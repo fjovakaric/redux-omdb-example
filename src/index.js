cd3e567145ada90c81d01c3d078ca4c86dcd75fc
fj <filip.jovakaric@gmail.com>
2016-08-16 23:09:50 +0200

    Update index.js and index.html #17

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('woohoo madafaka! The project is set!');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+
+import routes from './routes';
+
+render (
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('redux-omdb-example')
+);
