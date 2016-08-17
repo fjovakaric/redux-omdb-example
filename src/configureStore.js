d1f7af8fd65e0ab240c8fd216ec8f9c6a9938cc1
fj <filip.jovakaric@gmail.com>
2016-08-17 15:15:56 +0200

    Configure redux store #27

---
+++ src/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import rootReducer from './root.reducer';
+
+export default function configureStore(initialState) {
+    return createStore(
+        rootReducer,
+        initialState
+    );
+}
