14f4600b5a310c49c89618da835d306f053e191a
fj <filip.jovakaric@gmail.com>
2016-08-17 13:51:52 +0200

    Create MoviesPage component #23 - basic component added

---
+++ src/pages/movies/movies.container.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+import { } from 'react-bootstrap';
+
+import './_movies.scss'
+
+class MoviesPage extends React.Component {
+    render() {
+        return (
+            <div className="movies-page">
+                <h1>Movies</h1>
+            </div>
+        );
+    }
+}
+
+MoviesPage.propTypes = {
+};
+
+export default MoviesPage;
