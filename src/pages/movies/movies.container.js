710849e5c640e33c6d08017261a0ab7a6b326480
fj <filip.jovakaric@gmail.com>
2016-08-17 15:20:34 +0200

    Connect MoviesPage to Redux #33

--- src/pages/movies/movies.container.js
+++ src/pages/movies/movies.container.js
@@ -1,19 +1,42 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { } from 'react-bootstrap';
 
 import './_movies.scss'
 
 class MoviesPage extends React.Component {
     render() {
+        const { movies } = this.props;
+
+        const moviePosters = movies.map((movie) => {
+            return (
+                <div className="movie-preview" key={movie.imdbID}>
+                    <img src={movie.Poster} alt=""/>
+                    <p>{movie.Title}</p>
+                </div>
+            )
+        });
         return (
             <div className="movies-page">
                 <h1>Movies</h1>
+                <div className="display-movies">
+                    {moviePosters}
+                </div>
             </div>
         );
     }
 }
 
 MoviesPage.propTypes = {
+    movies: PropTypes.array
 };
 
-export default MoviesPage;
+function mapStateToProps(state, ownProps) {
+    return {
+        movies: state.movies.movies
+    };
+}
+
+export default connect(
+    mapStateToProps
+)(MoviesPage);
