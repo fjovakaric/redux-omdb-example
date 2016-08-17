83c0f2e66a11f8bb4c7aa7f4687cc5a8421e55f9
fj <filip.jovakaric@gmail.com>
2016-08-17 13:52:03 +0200

    Create MoviesPage component #23 - page added to routes

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './common-components/app/app';
 import HomePage from './pages/home/home.container';
+import MoviesPage from './pages/movies/movies.container';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
+        <Route path="/movies" component={MoviesPage}/>
     </Route>
 );
