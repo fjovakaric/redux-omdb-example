901bacfae2642198fa5c129daa6ae035f74c7827
fj <filip.jovakaric@gmail.com>
2016-08-17 15:22:17 +0200

    Setup Redux in index.js #25

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router';
 
+import configureStore from './configureStore';
 import routes from './routes';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './main.scss';
 
+const store = configureStore();
+
 render (
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('redux-omdb-example')
 );
