1c4c69fc2b555e66590803c2f5b526b3d2dad3b0
fj <filip.jovakaric@gmail.com>
2016-08-17 14:11:51 +0200

    Create Header component #24 - Header added to App component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,14 @@
 import React, {PropTypes} from 'react';
 
+import Header from '../header/header';
+
 import './_app.scss';
 
 class App extends React.Component {
     render() {
         return (
             <div className="app">
+                <Header />
                 {this.props.children}
             </div>
         );
