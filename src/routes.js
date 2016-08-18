a76a82bd43970fcc680e1a98b0b176db680978f7
fj <filip.jovakaric@gmail.com>
2016-08-18 15:59:17 +0200

    Add movie route #59

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app/app';
 import HomePage from './pages/home/home.container';
 import MoviesPage from './pages/movies/movies.container';
+import MoviePage from './pages/movie/movie.container';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="/movies" component={MoviesPage}/>
+        <Route path="/movie/:id" component={MoviePage}/>
     </Route>
 );
