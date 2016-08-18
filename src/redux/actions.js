915106852fa12e97a0db8ebe5af04b16cafba430
fj <filip.jovakaric@gmail.com>
2016-08-18 12:51:22 +0200

    Create async searchMovies action #50 - actions added

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,28 @@
 import actionTypes from './constants';
 
+import * as moviesApi from '../api/movies.api';
+
+export function searchMovies(movieName) {
+    return (dispatch) => {
+        dispatch(beginAjaxCall());
+        moviesApi
+            .search(movieName)
+            .then((result) => {
+                dispatch(searchMoviesSuccess(result.data.Search));
+            })
+            .catch((err) => {
+                dispatch(ajaxCallError(err));
+            });
+    }
+}
+
+export function searchMoviesSuccess(movies) {
+    return {
+        type: actionTypes.SEARCH_MOVIES_SUCCESS,
+        movies
+    }
+}
+
 export function beginAjaxCall() {
     return {
         type: actionTypes.BEGIN_AJAX_CALL
