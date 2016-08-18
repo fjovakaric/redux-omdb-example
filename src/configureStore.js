4f8d736ad5afc8cffe17234e42792fdd5ef6a5e1
fj <filip.jovakaric@gmail.com>
2016-08-18 12:18:21 +0200

    Add redux-thunk to project #45 - thunk middleware added to store

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './root.reducer';
+
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
         compose(
+            applyMiddleware(thunk),
             window.devToolsExtension ? window.devToolsExtension() : f => f
         )
     );
